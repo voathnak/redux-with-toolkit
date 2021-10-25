1314b814ce9b1d76c7b141c82562a84ef643c02e
Voathnak Lim <voathnak.lim@rhbgroup.com>
2021-10-25 12:00:51 +0700

    changed from react hook state to redux

--- src/Components/CountingNumber.js
+++ src/Components/CountingNumber.js
@@ -1,14 +1,18 @@
 // import 'style.css';
 
-import {useState} from "react";
+import {useDispatch, useSelector} from "react-redux";
+import {decrement, increment} from "../redux/counter";
 
 function CountingNumber() {
-  const [count, setCount] = useState(0);
+  const { count }  = useSelector((state) => state.counter);
+  const dispatch =  useDispatch();
+  const increaseCount = () => dispatch(increment());
+  const decreaseCount = () => dispatch(decrement());
   return (
     <div>
-      <p className={"text"}>Counting Number: <span>{count}</span></p>
-      <button onClick={() => setCount(count + 1)}>increasing</button>
-      <button  onClick={() => setCount(count - 1)}>decreasing</button>
+      <p className={"text"}>Counting Number: <span>{ count }</span></p>
+      <button onClick={increaseCount}>increasing</button>
+      <button  onClick={decreaseCount}>decreasing</button>
     </div>
   );
 }
